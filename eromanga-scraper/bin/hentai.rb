1251fb04a21f3becc1df0aa3b70e2a92c804603b
mapserver2007 <mapserver2007@gmail.com>
2020-08-12 14:50:59 +0900

    Ruby2.7、rubocop、solargraph適用

--- eromanga-scraper/bin/hentai.rb
+++ eromanga-scraper/bin/hentai.rb
@@ -1,18 +1,14 @@
-# -*- coding: utf-8 -*-
-$: << File.dirname(__FILE__) + "/../lib"
+$LOAD_PATH << File.dirname(__FILE__) + '/../lib'
 require 'download'
 require 'optparse'
 require 'yaml'
 
 config = {}
 OptionParser.new do |opt|
-  opt.on('-f', '--file FILE') {|v| config[:file] = v}
+  opt.on('-f', '--file FILE') { |v| config[:file] = v }
   opt.parse!
 end
 
 obj = YAML.load_file(config[:file]) if File.exist?(config[:file])
-dir = obj["dir"]
-url_list = obj["url_list"]
-
-hentai = Hentai::Download.new(dir)
-hentai.download(url_list)
+hentai = Hentai::Download.new(obj['paths'])
+hentai.download
--- eromanga-scraper/lib/download.rb
+++ eromanga-scraper/lib/download.rb
@@ -1,33 +1,37 @@
-# -*- coding: utf-8 -*-
 require 'mechanize'
 require 'open-uri'
 require 'parallel'
+require 'fileutils'
 
 module Hentai
-  REFERER = 'http://www.google.com/'
-  USER_AGENT = 'Mozilla/5.0 (Android; Mobile; rv:21.0) Gecko/21.0 Firefox/21.0'
+  REFERER = 'http://www.google.com/'.freeze
+  USER_AGENT = 'Mozilla/5.0 (Android; Mobile; rv:21.0) Gecko/21.0 Firefox/21.0'.freeze
 
   class Download
-    def initialize(dir)
-      @dir = dir
+    def initialize(paths)
+      @paths = paths
       @agent = Mechanize.new
       @agent.user_agent = USER_AGENT
       @agent.read_timeout = 180
     end
 
-    def download(url_list)
-      unless Dir.exist?(@dir) then
-        puts "#{@dir}は存在しません"
-        return
-      end
-
-      url_list.each do |url_info|
-        save_dir = get_save_dir(url_info["name"])
-        if save_dir.nil?
-          puts "保存ディレクトリが作成できませんでした"
-          next
+    def download
+      @paths.each do |path, urls|
+        unless Dir.exist?(path)
+          puts "#{path}は存在しません\n"
+          puts "#{path}を作成します\n"
+          FileUtils.mkdir_p(path)
+          exit
         end
-        get_url_pages(url_info["url"], save_dir)
+
+        urls['urls'].each do |url_info|
+          save_dir = get_save_dir(url_info['name'], path)
+          if save_dir.nil?
+            puts '保存ディレクトリが作成できませんでした'
+            next
+          end
+          get_url_pages(url_info['url'], save_dir)
+        end
       end
     end
 
@@ -38,20 +42,20 @@ module Hentai
         Parallel.each(url_list, in_threads: 3) do |url_page|
           get_image_link_url(url_page) do |img_url|
             puts img_url
-            save_image(save_dir, img_url, {"Referer" => REFERER})
+            save_image(save_dir, img_url, { 'Referer' => REFERER })
           end
         end
       when /eromanga-mainichi\.com/
         url_list = get_url_pages_eromanga_everyday(url)
         Parallel.each(url_list, in_threads: 3) do |img_url|
           puts img_url
-          save_image(save_dir, img_url, {"Referer" => REFERER})
+          save_image(save_dir, img_url, { 'Referer' => REFERER })
         end
       when /nhentai\.net/
         url_list = get_url_pages_nhentai(url)
         Parallel.each(url_list, in_threads: 3) do |img_url|
           puts img_url
-          save_image(save_dir, img_url, {"Referer" => "https://i.nhentai.net/"})
+          save_image(save_dir, img_url, { 'Referer' => 'https://i.nhentai.net/' })
         end
       end
     end
@@ -59,10 +63,10 @@ module Hentai
     def get_url_pages_eromanga_everyday(url)
       site = @agent.get(url)
       url_list = []
-      lines = (site/ "//div[@id='main']/div/div[1]").search('img')
+      lines = (site / "//div[@id='main']/div/div[1]").search('img')
       lines.each do |line|
         url = line.attribute('src').value
-        url_list << url if /img\.eromanga-mainichi\.com/ =~ url
+        url_list << url if /img\.eromanga-mainichi\.com/.match?(url)
       end
 
       url_list
@@ -70,11 +74,11 @@ module Hentai
 
     def get_url_pages_e_hentai(url)
       site = @agent.get(url)
-      lines = (site/ "//table[@class='ptt']/tr/td/a")
+      lines = (site / "//table[@class='ptt']/tr/td/a")
       max_page = 0
       lines.each do |line|
         text = line.inner_html
-        max_page = text.to_i if /\d+/ =~ text
+        max_page = text.to_i if /\d+/.match?(text)
       end
 
       url_list = [url]
@@ -88,12 +92,12 @@ module Hentai
     def get_url_pages_nhentai(url)
       url_list = []
       site = @agent.get(url)
-      lines = (site/ "//div[@class='thumb-container']/a/img")
+      lines = (site / "//div[@class='thumb-container']/a/img")
       lines.each do |line|
         text = line.attribute('data-src').value
         list = text.split('/')
         if /(\d+)t\.(.+)/ =~ list[5]
-          url_list << "https://i.nhentai.net/galleries/%s/%s.%s" % [list[4], $~[1], $~[2]]
+          url_list << `https://i.nhentai.net/galleries/%s/%s.%s` % [list[4], $LAST_MATCH_INFO[1], $LAST_MATCH_INFO[2]]
         end
       end
 
@@ -103,9 +107,9 @@ module Hentai
     def get_image_link_url(url)
       get_content_page(url) do |lines|
         lines.each do |line|
-          site = @agent.get(line["href"])
-          (site/ "//img[@id='img']").each do |real_line|
-            yield real_line["src"] if /^https?/ =~ real_line["src"]
+          site = @agent.get(line['href'])
+          (site / "//img[@id='img']").each do |real_line|
+            yield real_line['src'] if /^https?/.match?(real_line['src'])
           end
         end
       end
@@ -113,33 +117,29 @@ module Hentai
 
     def get_content_page(url)
       site = @agent.get(url)
-      lines = (site/ "//div[@class='gdtm']//a")
-      if lines.size == 0
-        site = @agent.get(url + "?nw=session")
-        lines = (site/ "//div[@class='gdtm']//a")
+      lines = (site / "//div[@class='gdtm']//a")
+      if lines.size.zero?
+        site = @agent.get(url + '?nw=session')
+        lines = (site / "//div[@class='gdtm']//a")
       end
 
       yield lines
     end
 
     def save_image(dir, url, options)
-      begin
-        open(url, options) do |f|
-          open(dir + "/" + File.basename(url), 'wb') do |output|
-            output.write(f.read)
-          end
+      OpenURI.open_uri(url, options) do |f|
+        File.open(dir + '/' + File.basename(url), 'wb') do |output|
+          output.write(f.read)
         end
-      rescue => e
-        puts e.message
       end
+    rescue StandardError => e
+      puts e.message
     end
 
-    def get_save_dir(name)
-      dir_path = @dir + "/" + name
+    def get_save_dir(name, path)
+      dir_path = path + '/' + name
       unless Dir.exist?(dir_path)
-        if Dir.mkdir(dir_path) != 0
-          return nil
-        end
+        return nil if Dir.mkdir(dir_path) != 0
       end
 
       dir_path
